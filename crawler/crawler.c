12a487fd39a0299dffdcf5ac2d161e5135d1140a
neuromanqe <agonhoxha99@gmail.com>
2021-10-19 11:38:18 -0400

    on step 5 now

--- crawler/crawler.c
+++ crawler/crawler.c
@@ -20,12 +20,26 @@ void printurl(void* pagep) {
 	printf("%s-->\n", webpage_getURL((webpage_t*)pagep));
 }
 
+bool search(void* elementp, const void* searchkeyp) {
+
+	char *ep = (char*)elementp;
+	char *sp = (char*)searchkeyp;
+
+	if(strcmp(ep, sp) == 0)
+		return true;
+
+	return false;
+	
+}
+
 int main(void) {
 	webpage_t *webpage, *tmp_webpage;
 	char *url;
 	int pos = 0;
 	char *result;
 	queue_t *urls_to_visit_qp;
+	hashtable_t *visited_urls_htp;
+	char* visited_url;
 	
 	url = "https://thayer.github.io/engs50/";
 		
@@ -37,21 +51,30 @@ int main(void) {
 	}
 
 	urls_to_visit_qp = qopen();
+	visited_urls_htp = hopen(10);
 	
 	while ((pos = webpage_getNextURL(webpage, pos, &result)) > 0) {
 		if(IsInternalURL(result)) {
-			printf("[INTERNAL]: %s\n", result);
+			//printf("[INTERNAL]: %s\n", result);
 			tmp_webpage = webpage_new(result, 0, NULL);
-			qput(urls_to_visit_qp, tmp_webpage); 
-		} else {
+			visited_url = webpage_getURL(tmp_webpage);
+			
+			if(hsearch(visited_urls_htp, search, visited_url, strlen(visited_url)) == NULL) {
+				hput(visited_urls_htp, visited_url, visited_url, strlen(visited_url));
+				qput(urls_to_visit_qp, tmp_webpage);
+			} else free(tmp_webpage);
+
+		}/* else {
 			printf("[EXTERNAL]: %s\n", result);		
-		}
+			}*/
 		free(result);
 	}
-	
+
+	free(visited_url);
 	qapply(urls_to_visit_qp, printurl);
 	qapply(urls_to_visit_qp, webpage_delete);
 	qclose(urls_to_visit_qp);
+	hclose(visited_urls_htp);
 	
 	webpage_delete(webpage);
 	exit(EXIT_SUCCESS);
