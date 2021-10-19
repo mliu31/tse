9a410ccb362aa2e5bc11a8940858999c1e41fb42
Megan Liu <megan.y.liu.24@dartmouth.edu>
2021-10-18 21:16:32 -0400

    step4 progress

--- crawler/crawler.c
+++ crawler/crawler.c
@@ -19,16 +19,25 @@
 int main(void) {
 	webpage_t *webpage;
 	char *url;
-
+	int pos = 0;
+	char *result;
+	
 	url = "https://thayer.github.io/engs50/";
 		
 	webpage = webpage_new(url, 0, NULL); 
 
-	if(webpage_fetch(webpage)) {
+	/*	if(webpage_fetch(webpage)) {
 		char *html = webpage_getHTML(webpage);
 		printf("Found html: %s\n", html);
 		exit(EXIT_SUCCESS); 
 	}
+	*/
+	while ((pos = webpage_getNextURL(webpage, pos, &result)) > 0) {
+		printf("Found url: %s\n", result);
+		free(result);
+	}
+	
+	
 	exit(EXIT_FAILURE); 
 	
 	webpage_delete(webpage); 
