e81e34b1d00408f4a3226e65eb1cbeb4584e8579
Megan Liu <megan.y.liu.24@dartmouth.edu>
2021-10-22 15:20:14 -0400

    strcpy visited_url but still buggy

--- crawler/crawler.c
+++ crawler/crawler.c
@@ -6,7 +6,7 @@
  * Version: 1
  * 
  * Description: 
- * 
+ * crawler implemented in C that crawls from a seed url to a specified depth 
  */
 
 #include <stdlib.h>
@@ -16,24 +16,24 @@
 #include <hash.h>
 #include <webpage.h>
 
+
 void printurl(void* pagep) {
 	printf("%s-->\n", webpage_getURL((webpage_t*)pagep));
 }
 
-bool search(void* elementp, const void* searchkeyp) {
 
+bool search(void* elementp, const void* searchkeyp) {
 	char *ep = (char*)elementp;
 	char *sp = (char*)searchkeyp;
 
-	if(strcmp(ep, sp) == 0)
+	if(strcmp(ep, sp) == 0) {
 		return true;
-
+	}
 	return false;
-	
 }
 
-int32_t pagesave(webpage_t *pagep, int id, char *dirname) {
 
+int32_t pagesave(webpage_t *pagep, int id, char *dirname) {
 	FILE *fp;
 	char path[64];
 	char name[64];
@@ -58,112 +58,86 @@ int32_t pagesave(webpage_t *pagep, int id, char *dirname) {
 	return 0;
 }
 
+
 int main(void) {
 	webpage_t *rootpage, *tmp_webpage, *webpage;
-	char *seedurl, *url;
-	int pos = 0;
-	int depth = 0;
-	int maxdepth;
-	char *result;
-	char* directory;
+	char *seedurl, *visited_url; 
+	int maxdepth, pos=0, depth=0, id=1; 
+	char *result, *directory;
 	queue_t *urls_to_visit_qp;
 	hashtable_t *visited_urls_htp;
-	char* visited_url;
-	int id = 1, counter = 0; 
+
 	seedurl = "https://thayer.github.io/engs50/";
 	directory = "../pages/";
-	maxdepth = 1;
+	maxdepth = 2;
 
 	rootpage = webpage_new(seedurl, depth, NULL);
 
 	urls_to_visit_qp = qopen();
 	visited_urls_htp = hopen(10);
 
-	hput(visited_urls_htp, seedurl, seedurl, strlen(seedurl));
 	qput(urls_to_visit_qp, rootpage);
+	hput(visited_urls_htp, seedurl, seedurl, strlen(seedurl));
 
-
-	
-	while(webpage = qget(urls_to_visit_qp)) {
-
+	while((webpage = qget(urls_to_visit_qp))) {
 		if(!webpage_fetch(webpage)) {
 			printf("Failed to fetch webpage: %s\n", webpage_getURL(webpage));
 			continue; 
 		}
+		
+		pagesave(webpage, id, directory);
 
-		if(hsearch(visited_urls_htp, search, webpage_getURL(webpage), strlen(webpage_getURL(webpage))) == NULL) {
-			pagesave(webpage, id, directory);
-			id = id + 1;
-		}
+		printf("id: %d\n", id);
+		id = id + 1;
+				
 		if(webpage_getDepth(webpage) < maxdepth) {
 			depth = webpage_getDepth(webpage) + 1;
 
 			while ((pos = webpage_getNextURL(webpage, pos, &result)) > 0) {
 				if(IsInternalURL(result)) {
-					//visited_url = NULL;
+					tmp_webpage = webpage_new(result, depth, NULL);					
+
+					// visited_url = NULL;
 					//visited_url = (char*)malloc(200*sizeof(char));
-
-					tmp_webpage = webpage_new(result, depth, NULL);
-					
-					// visited_url = webpage_getURL(tmp_webpage);
-
-					// printf("    result: %s; %s\n", result, webpage_getURL(tmp_webpage)); //, visited_url);
-
+					visited_url = webpage_getURL(tmp_webpage);  // need to initialize but should be overwritten by strcpy (shouldn't matter anyway) -- or does it overwrite what's at that address for the webpage URL? 
 					strcpy(visited_url, webpage_getURL(tmp_webpage));
+					
+					/*
+					if(hsearch(visited_urls_htp, search, webpage_getURL(tmp_webpage), strlen(webpage_getURL(tmp_webpage))) == NULL) {
+						hput(visited_urls_htp, webpage_getURL(tmp_webpage), webpage_getURL(tmp_webpage), strlen(webpage_getURL(tmp_webpage)));
+						qput(urls_to_visit_qp, tmp_webpage); 
+					} else 
+						webpage_delete(tmp_webpage);
+					*/
 					
 					if(hsearch(visited_urls_htp, search, visited_url, strlen(visited_url)) == NULL) {
 						hput(visited_urls_htp, visited_url, visited_url, strlen(visited_url));
 						qput(urls_to_visit_qp, tmp_webpage); 
+						printf(" %s  ==================== put into queue and hash table\n", visited_url);
+						// printf(" %s  ==================== put into queue and hash table\n", webpage_getURL(tmp_webpage));
 					} else {
 						webpage_delete(tmp_webpage);
 					}
+					
 				}
 				free(result);
-			
-			
 			}
-			// free(visited_url);			
+			// free(visited_url);	
 		}
-
 		
-		printf("depth after increment: %d\n", depth);
+		printf("depth of popped off webpg from queue: %d\n", depth);
 		
 		pos = 0;
-		printf("***********************\n"); 
+		printf("**********************************\n"); 
 		qapply(urls_to_visit_qp, printurl);
-		printf("***********************\n"); 
+		printf("**********************************\n"); 
 		//qapply(urls_to_visit_qp, webpage_delete);
 		
-
 		webpage_delete(webpage);
-	
-	
+	}
 		
-	}
-	/*	
-	while ((pos = webpage_getNextURL(webpage, pos, &result)) > 0) {
-		if(IsInternalURL(result)) {
-			tmp_webpage = webpage_new(result, 0, NULL);
-			visited_url = webpage_getURL(tmp_webpage);
-			
-			if(hsearch(visited_urls_htp, search, visited_url, strlen(visited_url)) == NULL) {
-				hput(visited_urls_htp, visited_url, visited_url, strlen(visited_url));
-				qput(urls_to_visit_qp, tmp_webpage);
-			} else free(tmp_webpage);
-
-		}
-		free(result);
-		}
-
-	free(visited_url);
-	*/
-	if(qget(urls_to_visit_qp) != NULL) {
-
-		printf("queue not empty\n");
-
-	}
 	qclose(urls_to_visit_qp);
 	hclose(visited_urls_htp);
- 
-	exit(EXIT_SUCCESS);
+
+	exit(EXIT_SUCCESS); 
 }
