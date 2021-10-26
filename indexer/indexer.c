39581e5f9c344bfa6427a9f026a193e667b90ccb
neuromanqe <agonhoxha99@gmail.com>
2021-10-26 19:31:36 -0400

    step 2 buggy

---
+++ indexer/indexer.c
@@ -0,0 +1,39 @@
+/* indexer.c --- 
+ * 
+ * 
+ * Author: Agon Hoxha
+ * Created: Tue Oct 26 19:04:11 2021 (-0400)
+ * Version: 
+ * 
+ * Description: 
+ * 
+ */
+
+#include <stdio.h>
+#include <pageio.h>
+#include <webpage.h>
+
+int main(void) {
+
+	webpage_t *loadedpage;
+	char *word;
+	int pos = 0;
+
+	printf("Hello!\n");
+
+	loadedpage = pageload(1, "../pages/");
+
+	while(pos != webpage_getHTMLlen(loadedpage)) {
+
+		pos = webpage_getNextWord(loadedpage, pos, &word);
+		printf("%s\n", word);
+
+		free(word);
+
+	}
+
+	webpage_delete(loadedpage);
+
+	return 0;
+
+}
