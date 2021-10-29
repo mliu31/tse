2c23f870a90c7c48be9f974c4f117135409c95bb
Megan Liu <megan.y.liu.24@dartmouth.edu>
2021-10-29 11:18:31 -0400

    step6 progress

---
+++ utils/indexio.c
@@ -0,0 +1,31 @@
+/* indexio.c --- 
+ * 
+ * 
+ * Author: Megan Y. Liu
+ * Created: Fri Oct 29 10:38:09 2021 (-0400)
+ * Version: 
+ * 
+ * Description: 
+ * 
+ */
+
+#include <stdio.h>
+#include <ctype.h>
+#include <queue.h>
+#include <hash.h>
+#include <pageio.h>
+#include <indexio.h>
+
+
+void indexsave(hash_table *index, char *filepath) {
+	FILE *file;
+
+	file = fopen(filepath, "w+");
+	
+	fprintf(); 
+}
+
+
+hashtable_t* indexload(char *filepath) {
+	; 
+}
---
+++ utils/indexio.h
@@ -0,0 +1,38 @@
+/* indexio.h --- 
+ * 
+ * 
+ * Author: Megan Y. Liu
+ * Created: Fri Oct 29 10:13:31 2021 (-0400)
+ * Version: 
+ * 
+ * Description: 
+ * header file for indexio with two functions (1) indexsave() and (2) indexload() 
+ * 
+ * (1) save an index to a named file indexnm following the synatax <word> <docID1> count<1> <docID2> <count2> ... <docIDN> <countN>
+ * (2) load a file from indexnm in the above syntax and returns a hashtable index  
+ */
+
+typedef struct index_entry_t {
+	char *word;
+	queue_t *word_queue_p;
+} idxe_t;
+
+
+typedef struct word_queue_entry_t {
+	int doc_id;
+	int doc_qord_freq;
+} wqe_t;
+
+
+/* 
+	 in: hashtable index
+	 out: output to indexnm file in the format described above 
+ */
+void indexsave(hashtable_t *index, char *filepath); 
+
+
+/*
+	in: indexnm file in the format described above 
+	out: hashtable index of the data in indexm
+*/
+hashtable_t* indexload(char *filepath); 
