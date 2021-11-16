1864464395e75cad5057fc07b2eec7778e9e0eb9
neuromanqe <agonhoxha99@gmail.com>
2021-11-16 09:49:04 -0500

    step 3 complete?

--- indexer/lindexer-1thread.c
+++ indexer/lindexer-1thread.c
@@ -27,7 +27,6 @@ typedef struct thread_container {
 	int page_id; 
 } thread_t; 
 
-
 static wqe_t* makeQueueEntry(int id) {
 	wqe_t* queue_entry = (wqe_t*)calloc(1, sizeof(wqe_t));
 
@@ -36,7 +35,6 @@ static wqe_t* makeQueueEntry(int id) {
 
 	return queue_entry;
 }
-
 
 static idxe_t* makeindexentry(char *word, int id) {
 	idxe_t* indexentry = malloc(sizeof(idxe_t));
@@ -49,17 +47,14 @@ static idxe_t* makeindexentry(char *word, int id) {
 	return indexentry;
 }
 
-
 static void incrementDocumentFreq(wqe_t *document) {
 	document->doc_word_freq = (document->doc_word_freq) + 1;
 }
-
 
 static void freeDocument(void *document) {
 	wqe_t *document_el = (wqe_t*)(document);
 	free(document_el);
 }
-
 
 static void freeindexentry(void *indexentry) {
 	idxe_t *index_e = (idxe_t*)(indexentry);
@@ -70,7 +65,6 @@ static void freeindexentry(void *indexentry) {
 	free(index_e->word);
 	free(index_e);
 }
-
 
 static int NormalizeWord(char **wordptr) {
 	char *word = *wordptr;
@@ -121,61 +115,56 @@ void* read_document_wbw(void *threadID) {
 	char *word;
 	webpage_t *loadedpage;
 	idxe_t *indexentry;
-	wqe_t *document;
-	
+	wqe_t *document;	
 	thread_t *tid = (thread_t*)threadID;
-	int id = tid->page_id;
-	lht_t *hashtable = tid->hashtable;
-	
-
-	pthread_mutex_lock(hashtable->mutex);
-	loadedpage = pageload(id, pagedirectory);
-	pthread_mutex_unlock(hashtable->mutex);
+	int id;
 	
 	while(true) {
 
 		pos = 0;
-		id = tid->page_id;
-	
-			while(true) {
-				pos = webpage_getNextWord(loadedpage, pos, &word);
-
-				if(pos == -1) 
-					break;
 		
-				if(NormalizeWord(&word) == 0) {
-					indexentry = lhsearch(hashtable, search, word, strlen(word));
-					if(indexentry == NULL) {
-						indexentry = makeindexentry(word, id);
-						lhput(hashtable, indexentry, word, strlen(word));
-					} else {
+		pthread_mutex_lock(tid->hashtable->mutex);
+		loadedpage = pageload(tid->page_id, pagedirectory);
+		id = tid->page_id;
+		(tid->page_id)++;
+		pthread_mutex_unlock(tid->hashtable->mutex);
 
-						// in entry, search its queue of docs
-						document = qsearch(indexentry->word_queue_p, search_queue, &id);
+		if(loadedpage == NULL) 
+			break;		
+	
+		while(true) {
 
-						// If this id found, increase freq
-						if(document == NULL) { // word found in new document
-							document = makeQueueEntry(id);
-							qput(indexentry->word_queue_p, document);
-						} else { // else put another document element in the queue
-							incrementDocumentFreq(document);
-						}
-						
-						free(word);
+			pos = webpage_getNextWord(loadedpage, pos, &word);
+				
+			if(pos == -1) {
+				break;
+			}
+		
+			if(NormalizeWord(&word) == 0) {
+				indexentry = lhsearch(tid->hashtable, search, word, strlen(word));
+				if(indexentry == NULL) {
+					indexentry = makeindexentry(word, id);
+					lhput(tid->hashtable, indexentry, word, strlen(word));
+				} else {
+
+					// in entry, search its queue of docs
+					document = qsearch(indexentry->word_queue_p, search_queue, &id);
+
+					// If this id found, increase freq
+					if(document == NULL) { // word found in new document
+						document = makeQueueEntry(id);
+						qput(indexentry->word_queue_p, document);
+					} else { // else put another document element in the queue
+						incrementDocumentFreq(document);
 					}
+						
+					free(word);
 				}
 			}
+		}
 
-			printf("id: %d\n", id);
-			webpage_delete(loadedpage);
-			
-			pthread_mutex_lock(hashtable->mutex);
-			(tid->page_id)++;			
-			loadedpage = pageload(id, pagedirectory);
-			pthread_mutex_unlock(hashtable->mutex);
-
-			if(loadedpage == NULL) 
-				break;
+		printf("id: %d\n", id);
+		webpage_delete(loadedpage);
 			
 	}
 	return 0; 
@@ -184,15 +173,15 @@ void* read_document_wbw(void *threadID) {
 
 int main(int argc, char *argv[]) {
 	char *indexnm;
-	
-	int num_threads = 1;
-	pthread_t threads[num_threads]; 
+	int num_threads;
 
-	if(argc != 3) { 
-		printf("usage: lindexer <pagedir> <indexfilenm>\n"); 
+
+	if(argc != 4) { 
+		printf("usage: lindexer <pagedir> <indexfilenm> <no_of_threads>\n"); 
 		return -1;
 	}
-	
+
+	num_threads = strtol(argv[3], NULL, 10);;
 	indexnm = argv[2];
 	pagedirectory = argv[1];
 
@@ -200,6 +189,7 @@ int main(int argc, char *argv[]) {
 	threadID->hashtable = lhopen(20);
 	threadID->page_id = 1;
 
+	pthread_t threads[num_threads]; 
 	//printf("loaded first page\n");
 
 	for(int i=0; i<num_threads; i++) { 
@@ -219,7 +209,6 @@ int main(int argc, char *argv[]) {
 	}
 
 	lindexsave(threadID->hashtable, indexnm); 
-
 	lhapply(threadID->hashtable, freeindexentry);
 	lhclose(threadID->hashtable);
 	free(threadID); 
